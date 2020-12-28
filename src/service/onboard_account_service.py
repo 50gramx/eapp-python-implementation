4086a0e88ee916968f62e0e144409d1583da9e43
cs145442 <amitkumarkhetan15@gmail.com>
2020-12-28 18:27:57 +0530

    updated protos and added temporary onboarding services

--- src/service/onboard_account_service.py
+++ src/service/onboard_account_service.py
@@ -9,8 +9,10 @@ from sqlalchemy import exists
 from sqlalchemy.exc import SQLAlchemyError
 
 from db_session import DbSession
+from ethos.elint.services.product.identity import onboard_account_pb2
 from ethos.elint.services.product.identity.onboard_account_pb2 import ClaimAccountResponse, \
-    ReRequestCodeClaimingAccountResponse, AuthenticateWithMobileNumberResponse
+    ReRequestCodeClaimingAccountResponse, VerifyClaimingAccountResponse, AuthenticateClaimedAccountResponse, \
+    AccessEthosAssistanceResponse
 from ethos.elint.services.product.identity.onboard_account_pb2_grpc import OnboardAccountServiceServicer
 from models.account_model import Account
 from support.application.registry import Registry
@@ -30,90 +32,100 @@ class OnboardAccountService(OnboardAccountServiceServicer):
     """
 
     def ClaimAccount(self, request, context):
-        # Getting the request params
-        try:
-            account_email_id = request.account_email_id
-            requested_at = request.requested_at
-        except Exception as err:
-            account_email_id = None
-            requested_at = None
-            logger.error("request, exception: {}".format(str(err)))
-        # Some helper flags
-        account_claimable = False
-        account_exists = None
-
-        # Validate email id
-        account_validated = validate_email_dns(account_email_id, check_mx=False, verify=False)
-
-        # Generate Session Token
-        onboard_session_token = str(uuid.uuid4())
-        Registry.register_data(onboard_session_token, [account_email_id, requested_at, onboard_session_token])
-
-        # check account exists in the accounts db
-        try:
-            if account_validated and account_validated is not None:
-                with DbSession.session_scope() as session:
-                    # Fetch the records
-                    exists_statement = exists().where(Account.account_email_id == account_email_id)
-                    result_set = session.query(Account).filter(exists_statement)
-                    session.commit()
-                    # Verify among the existing users
-                    for record in result_set:
-                        if record.account_email_id == account_email_id:
-                            logging.info(
-                                f"Warning: re-onboard req by existing user, {record.account_id} at {requested_at}.")
-                            account_exists = True
-                    if account_exists is not True and account_exists is None:
-                        account_exists = False
-        except SQLAlchemyError as err:
-            logger.error("SQLAlchemyError {}".format(str(err)))
-            context.set_code(grpc.StatusCode.UNKNOWN)
-
-        # Generate the boolean feedback about the claimability
-        if account_validated and not account_exists:
-            account_claimable = True
-
-        if account_claimable:
-
-            # Genreate the code, timing, token and register
-            verification_code, code_generated_at = get_random_string(6)
-            code_token = str(uuid.uuid4())
-            code_generated_at = format_time2timestamp(code_generated_at)
-            Registry.register_data(code_token, [verification_code, code_generated_at])
-
-            # Mail the onboarding account with Email Verification Token
-            mail_successful = mail(
-                from_email=identity_service_mail_id,
-                to_email=account_email_id,
-                subject=claim_account_verification_mail_subject,
-                html_content=claim_account_verification_mail_body.format(verification_code)
-            )
-
-            # Send back the response to client
-            claim_account_response = ClaimAccountResponse(
-                account_claimable=True,
-                account_email_id=account_email_id,
-                verification_code_token=code_token,
-                onboard_session_token=onboard_session_token,
-                code_sent_at=code_generated_at
-            )
-            logger.info(MessageToDict(claim_account_response, including_default_value_fields=True,
-                                      preserving_proto_field_name=True))
-            return claim_account_response
-
-        else:
-
-            # Send the response back to client with No Code Token, Not Claimable
-            claim_account_response = ClaimAccountResponse(
-                account_claimable=False,
-                account_email_id=account_email_id,
-                verification_code_token="NA",
-                onboard_session_token=onboard_session_token,
-                code_sent_at=format_time2timestamp(0)
-            )
-            logger.info(MessageToDict(claim_account_response, including_default_value_fields=True,
-                                      preserving_proto_field_name=True))
-            return claim_account_response
+        # # Getting the request params
+        # try:
+        #     account_email_id = request.account_email_id
+        #     requested_at = request.requested_at
+        # except Exception as err:
+        #     account_email_id = None
+        #     requested_at = None
+        #     logger.error("request, exception: {}".format(str(err)))
+        # # Some helper flags
+        # account_claimable = False
+        # account_exists = None
+        #
+        # # Validate email id
+        # account_validated = validate_email_dns(account_email_id, check_mx=False, verify=False)
+        #
+        # # Generate Session Token
+        # onboard_session_token = str(uuid.uuid4())
+        # Registry.register_data(onboard_session_token, [account_email_id, requested_at, onboard_session_token])
+        #
+        # # check account exists in the accounts db
+        # try:
+        #     if account_validated and account_validated is not None:
+        #         with DbSession.session_scope() as session:
+        #             # Fetch the records
+        #             exists_statement = exists().where(Account.account_email_id == account_email_id)
+        #             result_set = session.query(Account).filter(exists_statement)
+        #             session.commit()
+        #             # Verify among the existing users
+        #             for record in result_set:
+        #                 if record.account_email_id == account_email_id:
+        #                     logging.info(
+        #                         f"Warning: re-onboard req by existing user, {record.account_id} at {requested_at}.")
+        #                     account_exists = True
+        #             if account_exists is not True and account_exists is None:
+        #                 account_exists = False
+        # except SQLAlchemyError as err:
+        #     logger.error("SQLAlchemyError {}".format(str(err)))
+        #     context.set_code(grpc.StatusCode.UNKNOWN)
+        #
+        # # Generate the boolean feedback about the claimability
+        # if account_validated and not account_exists:
+        #     account_claimable = True
+        #
+        # if account_claimable:
+        #
+        #     # Genreate the code, timing, token and register
+        #     verification_code, code_generated_at = get_random_string(6)
+        #     code_token = str(uuid.uuid4())
+        #     code_generated_at = format_time2timestamp(code_generated_at)
+        #     Registry.register_data(code_token, [verification_code, code_generated_at])
+        #
+        #     # Mail the onboarding account with Email Verification Token
+        #     mail_successful = mail(
+        #         from_email=identity_service_mail_id,
+        #         to_email=account_email_id,
+        #         subject=claim_account_verification_mail_subject,
+        #         html_content=claim_account_verification_mail_body.format(verification_code)
+        #     )
+        #
+        #     # Send back the response to client
+        #     claim_account_response = ClaimAccountResponse(
+        #         account_claimable=True,
+        #         account_email_id=account_email_id,
+        #         verification_code_token=code_token,
+        #         onboard_session_token=onboard_session_token,
+        #         code_sent_at=code_generated_at
+        #     )
+        #     logger.info(MessageToDict(claim_account_response, including_default_value_fields=True,
+        #                               preserving_proto_field_name=True))
+        #     return claim_account_response
+        #
+        # else:
+        #
+        #     # Send the response back to client with No Code Token, Not Claimable
+        #     claim_account_response = ClaimAccountResponse(
+        #         account_claimable=False,
+        #         account_email_id=account_email_id,
+        #         verification_code_token="NA",
+        #         onboard_session_token=onboard_session_token,
+        #         code_sent_at=format_time2timestamp(0)
+        #     )
+        #     logger.info(MessageToDict(claim_account_response, including_default_value_fields=True,
+        #                               preserving_proto_field_name=True))
+        #     return claim_account_response
+        #
+        # TODO: Temporary Response
+        claim_account_response = ClaimAccountResponse(
+            account_details = request.account_details,
+            account_claim_status = onboard_account_pb2.AccountClaimStatus.ClAIMABLE,
+            verification_code_token = str(uuid.uuid4()),
+            onboard_session_token = str(uuid.uuid4()),
+            code_sent_at = format_time2timestamp(0)
+        )
+        return claim_account_response
 
     def ReRequestCodeClaimingAccount(self, request, context):
         # Get the data parameters from the request
@@ -148,9 +160,33 @@ class OnboardAccountService(OnboardAccountServiceServicer):
             code_sent_at=code_generated_at
         )
 
-    def AuthenticateWithMobileNumber(self, request, context):
-        authenticate_with_mobile_number_response = AuthenticateWithMobileNumberResponse(
-            verification_code_token="123213213213123",
-            code_sent_at=format_time2timestamp(0)
+    def VerifyClaimingAccount(self, request, context):
+        verify_claiming_account_response = VerifyClaimingAccountResponse(
+            account_details = request.account_details,
+            account_verified = True,
+            verification_message = "Successfully Verified",
+            onboard_session_token = request.onboard_session_token,
+            onboard_organization_name = "SELF",
+            verified_at = format_time2timestamp(0)
         )
-        return authenticate_with_mobile_number_response
+        return verify_claiming_account_response
+
+
+    def AuthenticateClaimedAccount(self, request, context):
+        authenticate_claimed_account_response = AuthenticateClaimedAccountResponse(
+            account_details = request.account_details,
+            account_authenticated = True,
+            onboard_session_token = request.onboard_session_token,
+            authenticated_at = format_time2timestamp(0)
+        )
+        return authenticate_claimed_account_response
+
+    def AccessEthosAssistance(self, request, context):
+        access_ethos_assistance_response = AccessEthosAssistanceResponse(
+            account_details = request.account_details,
+            onboard_session_token = request.onboard_session_token,
+            access_provided = True,
+            ethos_access_session_token = str(uuid.uuid4()),
+            access_provided_at = format_time2timestamp(0)
+        )
+        return access_ethos_assistance_response
