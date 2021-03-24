40d8ee237a9eb11a30d02527a0eb0d62b2a1d18c
cs145442 <amitkumarkhetan15@gmail.com>
2021-03-24 20:56:48 +0530

    added service IsAccountExistsWithMobile

--- src/service/account/discover_account_service.py
+++ src/service/account/discover_account_service.py
@@ -4,7 +4,7 @@ from ethos.elint.services.product.identity.account.discover_account_pb2 import G
 from ethos.elint.services.product.identity.account.discover_account_pb2_grpc import DiscoverAccountServiceServicer
 from services_caller.account_assistant_service_caller import get_account_assistant_by_account_caller
 from services_caller.account_service_caller import validate_account_services_caller
-from support.db_service import get_account
+from support.db_service import get_account, is_existing_account_mobile
 
 
 class DiscoverAccountService(DiscoverAccountServiceServicer):
@@ -26,3 +26,17 @@ class DiscoverAccountService(DiscoverAccountServiceServicer):
             return AccountAssistant()
         else:
             return get_account_assistant_by_account_caller(request.account)
+
+    def IsAccountExistsWithMobile(self, request, context):
+        print("DiscoverAccountService:IsAccountExistsWithMobile")
+        validation_done, validation_message = validate_account_services_caller(request.access_auth_details)
+        response_meta = ResponseMeta(meta_done=validation_done, meta_message=validation_message)
+        if validation_done is False:
+            return response_meta
+        else:
+            is_existing_account = is_existing_account_mobile(account_country_code=request.account_country_code,
+                                                             account_mobile_number=request.account_mobile_number)
+            if is_existing_account:
+                return ResponseMeta(meta_done=True, meta_message="Account exists.")
+            else:
+                return ResponseMeta(meta_done=False, meta_message="Account doesn't exists.")
