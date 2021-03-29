5c9374a38699d52f0bdff56ad4d285caba9b629d
cs145442 <amitkumarkhetan15@gmail.com>
2021-03-30 04:15:59 +0530

    added todo for GetAccountProfilePicture

--- src/service/account/discover_account_service.py
+++ src/service/account/discover_account_service.py
@@ -21,6 +21,16 @@ class DiscoverAccountService(DiscoverAccountServiceServicer):
             response_meta=ResponseMeta(meta_done=True, meta_message="Get complete.")
         )
 
+    # TODO: Complete this service implementation
+    # def GetAccountProfilePicture(self, request, context):
+    #     logging.info("DiscoverAccountService:GetAccountProfilePicture")
+    #     validation_done, validation_message = validate_account_services_caller(request.access_auth_details)
+    #     response_meta = ResponseMeta(meta_done=validation_done, meta_message=validation_message)
+    #     if validation_done is False:
+    #         return response_meta
+    #     else:
+    #         pass
+
     def GetAccountAssistant(self, request, context):
         logging.info("DiscoverAccountService:GetAccountAssistant")
         validation_done, validation_message = validate_account_services_caller(request)
