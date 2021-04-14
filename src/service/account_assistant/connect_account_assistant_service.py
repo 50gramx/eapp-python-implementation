21c38c1482485d3c4542bf3e5c8bf744d3285319
cs145442 <amitkumarkhetan15@gmail.com>
2021-04-15 00:08:13 +0530

    NE-134 #comment added copyrights to all the services

--- src/service/account_assistant/connect_account_assistant_service.py
+++ src/service/account_assistant/connect_account_assistant_service.py
@@ -1,3 +1,22 @@
+#   /*************************************************************************
+#   *
+#   * AMIT KUMAR KHETAN CONFIDENTIAL
+#   * __________________
+#   *
+#   *  [2017] - [2021] Amit Kumar Khetan
+#   *  All Rights Reserved.
+#   *
+#   * NOTICE:  All information contained herein is, and remains
+#   * the property of Amit Kumar Khetan and its suppliers,
+#   * if any.  The intellectual and technical concepts contained
+#   * herein are proprietary to Amit Kumar Khetan
+#   * and its suppliers and may be covered by U.S. and Foreign Patents,
+#   * patents in process, and are protected by trade secret or copyright law.
+#   * Dissemination of this information or reproduction of this material
+#   * is strictly forbidden unless prior written permission is obtained
+#   * from Amit Kumar Khetan.
+#   */
+
 from ethos.elint.entities.generic_pb2 import ResponseMeta
 from ethos.elint.services.product.identity.account_assistant.connect_account_assistant_pb2 import ConnectAccountResponse
 from ethos.elint.services.product.identity.account_assistant.connect_account_assistant_pb2_grpc import \
