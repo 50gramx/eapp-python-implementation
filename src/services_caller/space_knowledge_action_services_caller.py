c57e30e517f73fcf606cf41cce191e4ae381ca6e
cs145442 <amitkumarkhetan15@gmail.com>
2021-07-25 21:32:24 +0530

    fixed bug

--- src/services_caller/space_knowledge_action_services_caller.py
+++ src/services_caller/space_knowledge_action_services_caller.py
@@ -18,13 +18,17 @@
 #   */
 
 from application_context import ApplicationContext
+from ethos.elint.entities.space_knowledge_domain_pb2 import SpaceKnowledgeDomain
 from ethos.elint.services.product.action.space_knowledge_action_pb2 import AskQuestionRequest, DomainRankedAnswers
 from ethos.elint.services.product.identity.account_assistant.access_account_assistant_pb2 import \
     AccountAssistantServicesAccessAuthDetails
 
 
-def ask_question(access_auth_details: AccountAssistantServicesAccessAuthDetails, message: str) -> (
+def ask_question(access_auth_details: AccountAssistantServicesAccessAuthDetails, message: str,
+                 ask_particular_domain: bool, space_knowledge_domain: SpaceKnowledgeDomain) -> (
         bool, str, [DomainRankedAnswers]):
     stub = ApplicationContext.space_knowledge_action_service_stub()
-    response = stub.AskQuestion(AskQuestionRequest(access_auth_details=access_auth_details, message=message))
+    response = stub.AskQuestion(AskQuestionRequest(access_auth_details=access_auth_details, message=message,
+                                                   ask_particular_domain=ask_particular_domain,
+                                                   space_knowledge_domain=space_knowledge_domain))
     return response.response_meta.meta_done, response.response_meta.meta_message, response.domains_ranked_answers
