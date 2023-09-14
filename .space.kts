e501c31493d83593df3aa28cf34d5c16cd256b08
Amit Khetan <amit.khetan.70@50gramx.io>
2023-09-14 15:48:52 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,59 @@
+
+job("Build & Deploy Identity Services") {
+
+    startOn {
+        gitPush {
+            anyBranchMatching {
+                +"release-*"
+                +"master"
+                +"main"
+            }
+        }
+    }
+  
+    host("Build Olive") {
+        dockerBuildPush {
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
+            // context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile"
+            // build-time variables
+            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            // labels["vendor"] = "mycompany"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
+            tags {
+                // use current job run number as a tag - '0.0.run_number'
+                +"50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/eapp-identity-services:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+            }
+        }
+    }
+    
+    host("Deploy Identity Services") {
+
+      shellScript {
+        content = """
+          docker stop eapp-identity-services  # Stop the existing container
+          docker rm eapp-identity-services    # Remove the stopped container
+          docker run -d --restart=always -p 5000:80 --name eapp-identity-services \
+            50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/eapp-identity-services:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER
+        """
+      }
+    }
+    
+    // run this job only on
+    // a Windows worker
+    // that is tagged as 'pool-1'
+    requirements {
+        workerTags("windows-pool")
+    }
+}
