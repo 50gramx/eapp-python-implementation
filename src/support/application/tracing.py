b412830be79eda9e11874dc69ba7b566c16ab9ae
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-10-19 02:42:04 +0530

    add tracing support

---
+++ src/support/application/tracing.py
@@ -0,0 +1,32 @@
+#   /*************************************************************************
+#   *
+#   * AMIT KUMAR KHETAN CONFIDENTIAL
+#   * __________________
+#   *
+#   *  [2017] - [2021] Amit Kumar Khetan
+#   *  All Rights Reserved.
+#   *
+#   * NOTICE:  All information contained herein is, and remains
+#   * the property of Amit Kumar Khetan and its suppliers,
+#   * if any.  The intellectual and technical concepts contained
+#   * herein are proprietary to Amit Kumar Khetan
+#   * and its suppliers and may be covered by U.S. and Foreign Patents,
+#   * patents in process, and are protected by trade secret or copyright law.
+#   * Dissemination of this information or reproduction of this material
+#   * is strictly forbidden unless prior written permission is obtained
+#   * from Amit Kumar Khetan.
+#   */
+
+from jaeger_client import Config
+
+
+def init_tracer(service_name):
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+            'local_agent': {'reporting_host': 'localhost', 'reporting_port': '6831'},
+            'logging': True,
+        },
+        service_name=service_name,
+    )
+    return config.initialize_tracer()
