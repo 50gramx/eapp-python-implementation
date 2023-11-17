e943bc1a42c6cd27b9e4c33feaa730a6e8c859e8
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-11-18 02:58:37 +0530

    add sql injections

---
+++ sql/03-insert-universe.sql
@@ -0,0 +1,27 @@
+/*
+ *  /*************************************************************************
+ *  *
+ *  * AMIT KUMAR KHETAN CONFIDENTIAL
+ *  * __________________
+ *  *
+ *  *  [2017] - [2021] Amit Kumar Khetan
+ *  *  All Rights Reserved.
+ *  *
+ *  * NOTICE:  All information contained herein is, and remains
+ *  * the property of Amit Kumar Khetan and its suppliers,
+ *  * if any.  The intellectual and technical concepts contained
+ *  * herein are proprietary to Amit Kumar Khetan
+ *  * and its suppliers and may be covered by U.S. and Foreign Patents,
+ *  * patents in process, and are protected by trade secret or copyright law.
+ *  * Dissemination of this information or reproduction of this material
+ *  * is strictly forbidden unless prior written permission is obtained
+ *  * from Amit Kumar Khetan.
+ *
+ */
+*/
+
+
+-- insert_universe.sql
+
+INSERT INTO universe (universe_id, universe_name, universe_description, universe_big_bang_at)
+VALUES ('92d6e05d-9f99-4b27-8980-d2227e88f7f5', 'INDIA', 'Universe for the People of INDIA', '2020-02-14 00:00:00');
---
+++ sql/04-insert-galaxy.sql
@@ -0,0 +1,26 @@
+/*
+ *  /*************************************************************************
+ *  *
+ *  * AMIT KUMAR KHETAN CONFIDENTIAL
+ *  * __________________
+ *  *
+ *  *  [2017] - [2021] Amit Kumar Khetan
+ *  *  All Rights Reserved.
+ *  *
+ *  * NOTICE:  All information contained herein is, and remains
+ *  * the property of Amit Kumar Khetan and its suppliers,
+ *  * if any.  The intellectual and technical concepts contained
+ *  * herein are proprietary to Amit Kumar Khetan
+ *  * and its suppliers and may be covered by U.S. and Foreign Patents,
+ *  * patents in process, and are protected by trade secret or copyright law.
+ *  * Dissemination of this information or reproduction of this material
+ *  * is strictly forbidden unless prior written permission is obtained
+ *  * from Amit Kumar Khetan.
+ *
+ */
+*/
+
+-- insert_data.sql
+INSERT INTO Galaxy (galaxy_id, galaxy_name, universe_id, galaxy_created_at)
+VALUES ('3c57ae83-9d6b-4927-a23e-6d48eae37cfd', 'Milky Way', '92d6e05d-9f99-4b27-8980-d2227e88f7f5',
+        '2023-11-18 12:00:00');
