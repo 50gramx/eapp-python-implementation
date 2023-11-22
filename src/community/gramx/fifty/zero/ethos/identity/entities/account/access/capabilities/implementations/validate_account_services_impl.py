69cadbc39e234a8f336a88c602260ede63b46952
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-11-22 17:30:02 +0530

    add logs

--- src/community/gramx/fifty/zero/ethos/identity/entities/account/access/capabilities/implementations/validate_account_services_impl.py
+++ src/community/gramx/fifty/zero/ethos/identity/entities/account/access/capabilities/implementations/validate_account_services_impl.py
@@ -30,28 +30,35 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 def validate_account_services_impl(request: AccountServicesAccessAuthDetails, session_scope: str):
-    # validate request params
+    logging.info("will check account id")
     if request.account.account_id is "":
+        logging.info("account id is null, returning")
         return ValidateAccountServicesResponse(
             account_service_access_validation_done=False,
             account_service_access_validation_message="Invalid Request. This action will be reported."
         )
 
+    logging.info("account id is found")
     account_id = request.account.account_id
     # validate the account
+    logging.info("will get the account")
     if get_account(account_id=account_id).account_id != account_id:
         # create the response here
+        logging.info("account id mis-match, will return")
         return ValidateAccountServicesResponse(
             account_service_access_validation_done=False,
             account_service_access_validation_message="Requesting account is not legit. This action will be reported."
         )
     else:
+        logging.info("account id is safe")
         # validate the session
+        logging.info("will check the session")
         session_valid, session_valid_message = is_persistent_session_valid(
             request.account_services_access_session_token_details.session_token,
             account_id,
             session_scope
         )
+        logging.info("checked the session, will return")
         return ValidateAccountServicesResponse(
             account_service_access_validation_done=session_valid,
             account_service_access_validation_message=session_valid_message
