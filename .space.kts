7c88253ad918c9bb877bc2def5857527585eb82b
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-01-24 22:35:35 +0530

    add job to build proxy image

--- .space.kts
+++ .space.kts
@@ -137,3 +137,64 @@ job("Build & Deploy Python Implementations") {
         workerTags("windows-pool")
     }
 }
+
+job("Build Capabilities Proxy Image") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"Dockerfile.proxy"
+            }
+        }
+    }
+
+    // To check a condition, basically, you need a kotlinScript step
+    host(displayName = "Setup Version") {
+        kotlinScript { api ->
+            // Get the current year and month
+            val currentYear = (LocalDate.now().year % 100).toString().padStart(2, '0')
+            val currentMonth = LocalDate.now().monthValue.toString()
+
+            // Get the execution number from environment variables
+            val currentExecution = System.getenv("JB_SPACE_EXECUTION_NUMBER")
+
+            // Set the VERSION_NUMBER parameter
+            api.parameters["VERSION_NUMBER"] = "$currentYear.$currentMonth.$currentExecution"
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+
+    host("Build and push capabilities proxy image") {
+        dockerBuildPush {
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
+            // context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile.proxy"
+            // build-time variables
+            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            // labels["vendor"] = "mycompany"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
+            tags {
+                // use current job run number as a tag - '0.0.run_number'
+                +"50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/capabilities-proxy:{{ VERSION_NUMBER }}"
+                +"50gramx.registry.jetbrains.space/p/main/ethosindiacontainers/capabilities-proxy:latest"
+            }
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+}
