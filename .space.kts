c00e80f06b23332ce2a2ca10416a27a872f85184
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-03-09 20:39:28 +0530

    fix slack update

--- .space.kts
+++ .space.kts
@@ -163,11 +163,11 @@ job("Build & Deploy Python Implementations") {
     }
 
     container("Send Slack Update", image = "amazoncorretto:17-alpine") {
-        env["SLACK_BOT_TOKEN"] = Secrets("SLACK_BOT_TOKEN")
+        env["SLACK_OAUTH_BOT_TOKEN"] = Secrets("SLACK_OAUTH_BOT_TOKEN")
 
         kotlinScript { api ->
             val slack = Slack.getInstance()
-            val token = System.getenv("SLACK_BOT_TOKEN")
+            val token = System.getenv("SLACK_OAUTH_BOT_TOKEN")
             val version = api.parameters["VERSION_NUMBER"]
             val response = slack.methods(token).chatPostMessage { req ->
                 req.channel("#product-dev").text("Finished Building & Deploying Python Implementations v$version")
