43a4f13b773058d2ba884b61a36c405c5db69651
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-03-29 06:38:48 +0530

    fix file page create consumer

---
+++ src/community/gramx/fifty/zero/ethos/knowledge_spaces/entities/space_knowledge_domain_file_page/create/consumers/create_space_knowledge_domain_file_page_consumer.py
@@ -0,0 +1,42 @@
+#   /*************************************************************************
+#   *
+#   * AMIT KUMAR KHETAN CONFIDENTIAL
+#   * __________________
+#   *
+#   *  [2017] - [2021] Amit Kumar Khetan
+#   *  All Rights Reserved.
+#   *
+#   * NOTICE:  All information contained herein is, and remains
+#   * the property of Amit Kumar Khetan and its suppliers,
+#   * if any.  The intellectual and technical concepts contained
+#   * herein are proprietary to Amit Kumar Khetan
+#   * and its suppliers and may be covered by U.S. and Foreign Patents,
+#   * patents in process, and are protected by trade secret or copyright law.
+#   * Dissemination of this information or reproduction of this material
+#   * is strictly forbidden unless prior written permission is obtained
+#   * from Amit Kumar Khetan.
+#   */
+import logging
+import os
+
+import grpc
+from ethos.elint.services.product.knowledge.space_knowledge_domain_file.access_space_knowledge_domain_file_pb2 import \
+    SpaceKnowledgeDomainFileServicesAccessAuthDetails
+from ethos.elint.services.product.knowledge.space_knowledge_domain_file_page.create_space_knowledge_domain_file_page_pb2_grpc import \
+    CreateSpaceKnowledgeDomainFilePageServiceStub
+
+
+class CreateSpaceKnowledgeDomainFilePageConsumer:
+
+    @staticmethod
+    async def extract_pages_from_file(access_auth_details: SpaceKnowledgeDomainFileServicesAccessAuthDetails):
+        aio_grpc_host = os.environ['ERPC_AIO_HOST']
+        aio_grpc_port = os.environ['ERPC_AIO_PORT']
+        aio_host_ip = "{host}:{port}".format(host=aio_grpc_host, port=aio_grpc_port)
+
+        async with grpc.aio.insecure_channel(aio_host_ip) as channel:
+            stub = CreateSpaceKnowledgeDomainFilePageServiceStub(channel)
+            response = await stub.ExtractPagesFromFile(access_auth_details)
+        logging.info(f"extract_pages_from_file client, Response Received: {response}")
+
+        # TODO: call rest of the tasks
