547beabe8309a474b8f5fd8408707aa272856b28
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-07-01 13:14:07 +0530

    fix imports

--- src/community/gramx/fifty/zero/ethos/identity/entities/universe/service/create_universe_service.py
+++ src/community/gramx/fifty/zero/ethos/identity/entities/universe/service/create_universe_service.py
@@ -1,16 +1,19 @@
 from ethos.elint.services.product.identity.universe.create_universe_pb2_grpc import CreateUniverseServiceServicer
-from implementation.create_universe_impl import create_universe_impl
+
+from community.gramx.fifty.zero.ethos.identity.entities.universe.implementation.create_universe_impl import \
+    create_universe_impl
 from support.application.tracing import trace_rpc, PYTHON_IMPLEMENTATION_TRACER
+
 
 class CreateUniverseService(CreateUniverseServiceServicer):
     def __init__(self):
         super(CreateUniverseService, self).__init__()
         self.session_scope = self.__class__.__name__
         self.tracer = PYTHON_IMPLEMENTATION_TRACER
-    
+
     def __del__(self):
         self.tracer.close()
-    
+
     @trace_rpc()
     def CreateUniverse(self, request, context):
         return create_universe_impl(request=request)
--- src/community/gramx/fifty/zero/ethos/identity/entities/universe/service/delete_universe_service.py
+++ src/community/gramx/fifty/zero/ethos/identity/entities/universe/service/delete_universe_service.py
@@ -1,16 +1,19 @@
 from ethos.elint.services.product.identity.universe.delete_universe_pb2_grpc import DeleteUniverseServiceServicer
-from implementation.delete_universe_impl import delete_universe_impl
+
+from community.gramx.fifty.zero.ethos.identity.entities.universe.implementation.delete_universe_impl import \
+    delete_universe_impl
 from support.application.tracing import trace_rpc, PYTHON_IMPLEMENTATION_TRACER
+
 
 class DeleteUniverseService(DeleteUniverseServiceServicer):
     def __init__(self):
         super(DeleteUniverseService, self).__init__()
         self.session_scope = self.__class__.__name__
         self.tracer = PYTHON_IMPLEMENTATION_TRACER
-    
+
     def __del__(self):
         self.tracer.close()
-    
+
     @trace_rpc()
     def DeleteUniverse(self, request, context):
         return delete_universe_impl(request=request)
