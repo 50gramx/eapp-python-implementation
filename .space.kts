90b40f0ddd8942914e326a756c4fca8a7ca01169
amit.khetan.70 <amit.khetan.70@50gramx.io>
2024-07-03 18:37:12 +0530

    added job to only deploy

--- .space.kts
+++ .space.kts
@@ -293,6 +293,172 @@ job("Build Cache Invalidated Python Implementations Image") {
     }
 }
 
+job("Deploy Python Implementations") {
+
+    startOn {
+        gitPush {
+            enabled = false
+            anyBranchMatching {
+                +"release-*"
+                +"master"
+                +"main"
+            }
+        }
+    }
+
+   // To check a condition, basically, you need a kotlinScript step
+    host(displayName = "Setup Version") {
+        kotlinScript { api ->
+            // Get the current year and month
+            val currentYear = (LocalDate.now().year % 100).toString().padStart(2, '0')
+            val currentMonth = LocalDate.now().monthValue.toString()
+
+            // Get the execution number from environment variables
+            val currentExecution = System.getenv("JB_SPACE_EXECUTION_NUMBER")
+
+            // Set the VERSION_NUMBER parameter
+            api.parameters["VERSION_NUMBER"] = "$currentYear.$currentMonth.$currentExecution"
+
+            // Fetch Commit Messages
+            val workingDir = File("/mnt/space/work/eapp-python-implementation") // Specify your source code directory
+
+            val process = ProcessBuilder("git", "-C", workingDir.absolutePath, "rev-parse", "HEAD")
+                .redirectOutput(ProcessBuilder.Redirect.PIPE)
+                .start()
+
+            val output = process.inputStream.bufferedReader().readLine() ?: ""
+            val commitHash = output.trim()
+
+            println("Current commit hash: $commitHash")
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+
+    container("Schedule Deployment", image = "amazoncorretto:17-alpine") {
+        env["SLACK_OAUTH_BOT_TOKEN"] = Secrets("SLACK_OAUTH_BOT_TOKEN")
+
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.schedule(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("python-implementation-deployment"),
+                version = api.parameters["VERSION_NUMBER"],
+            )
+            val slack = Slack.getInstance()
+            val token = System.getenv("SLACK_OAUTH_BOT_TOKEN")
+            val version = api.parameters["VERSION_NUMBER"]
+            val response = slack.methods(token).chatPostMessage { req ->
+                req.channel("#product-dev").text("⚠️ Scheduled 🐍 Python v$version System Capability Deployment 🙏")
+            }
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+
+    container("Start Deployment", image = "amazoncorretto:17-alpine") {
+        env["SLACK_OAUTH_BOT_TOKEN"] = Secrets("SLACK_OAUTH_BOT_TOKEN")
+
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.start(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("python-implementation-deployment"),
+                version = api.parameters["VERSION_NUMBER"],
+                // automatically update deployment status based on a status of a job
+                syncWithAutomationJob = true
+            )
+            val slack = Slack.getInstance()
+            val token = System.getenv("SLACK_OAUTH_BOT_TOKEN")
+            val version = api.parameters["VERSION_NUMBER"]
+            val response = slack.methods(token).chatPostMessage { req ->
+                req.channel("#product-dev").text("🚨️ Started 🐍 Python v$version System Capability Deployment 🙏")
+            }
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+
+    host("Trigger Python Implementations Backups") {
+
+        shellScript {
+            content = """
+                # Trigger backups before bringing down the services
+
+                export CONTAINER_NAME='eapp-python-implementation-postgres-1'
+                export CID=$(docker ps -q -f status=running -f name=^/${"$"}CONTAINER_NAME$)
+                if [ ! "${"$"}CID" ]; then
+                    echo "PostgreSQL container is not running. Skipping backup."
+                else
+                    echo "PostgreSQL container is running. Backing up..."
+                    docker-compose exec postgres /bin/sh -c "sh /psql_backup.sh instant"
+                fi
+                unset CID
+
+                export CONTAINER_NAME='eapp-python-implementation-redis-1'
+                export CID=$(docker ps -q -f status=running -f name=^/${"$"}CONTAINER_NAME$)
+                if [ ! "${"$"}CID" ]; then
+                    echo "Redis container is not running. Skipping backup."
+                else
+                    echo "Redis container is running. Backing up..."
+                    docker-compose exec redis /bin/sh -c "sh /redis_backup.sh instant"
+                fi
+                unset CID
+            """
+        }
+
+        requirements {
+            workerTags("windows-pool")
+            workerTags("amitkumarkhetan15-user")
+        }
+    }
+
+    host("Deploy Python Implementations Containers") {
+
+        shellScript {
+            content = """
+                # Bring down the services
+                docker-compose down --remove-orphans
+
+                # Bring up the services
+                docker-compose up -d
+            """
+        }
+
+        requirements {
+            workerTags("windows-pool")
+            workerTags("amitkumarkhetan15-user")
+        }
+    }
+
+    container("Finish Deployment", image = "amazoncorretto:17-alpine") {
+        env["SLACK_OAUTH_BOT_TOKEN"] = Secrets("SLACK_OAUTH_BOT_TOKEN")
+
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.finish(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("python-implementation-deployment"),
+                version = api.parameters["VERSION_NUMBER"],
+            )
+            val slack = Slack.getInstance()
+            val token = System.getenv("SLACK_OAUTH_BOT_TOKEN")
+            val version = api.parameters["VERSION_NUMBER"]
+            val response = slack.methods(token).chatPostMessage { req ->
+                req.channel("#product-dev").text("👋 Deployed 🐍 Python v$version System Capabilities 🙏")
+            }
+            // to fail the deployment, use ...deployments.fail()
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+}
+
 job("Build Capabilities Proxy Image") {
     startOn {
         gitPush {
