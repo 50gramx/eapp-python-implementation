872627d81e6d2ff9ceaf9cf32b77560d224b1785
Patroa aditya <patroa@50gramx.io>
2024-11-09 06:54:19 +0000

    created new job for kubernetes proxy image

--- .space.kts
+++ .space.kts
@@ -441,6 +441,81 @@ job("Build Capabilities Proxy Image") {
     }
 }
 
+job("Build Capabilities Proxy kubernetes Image") {
+    startOn {
+        gitPush {
+            pathFilter {
+                +"Dockerfile-kubernetes.proxy"
+                +"envoy-kubernetes.yaml"
+            }
+            anyBranchMatching {
+                +"release-*"
+                +"master"
+                +"main"
+            }
+        }
+    }
+
+    // To check a condition, basically, you need a kotlinScript step
+    host(displayName = "Setup Version") {
+        kotlinScript { api ->
+            // Get the current year and month
+            val currentYear = (LocalDate.now().year % 100).toString().padStart(2, '0')
+            val currentMonth = LocalDate.now().monthValue.toString()
+
+            // Get the execution number from environment variables
+            val currentExecution = System.getenv("JB_SPACE_EXECUTION_NUMBER")
+
+            // Set the VERSION_NUMBER parameter
+            api.parameters["VERSION_NUMBER"] = "$currentYear.$currentMonth.$currentExecution"
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+
+    host("Build and push capabilities proxy image") {
+        
+        shellScript {
+            content = """
+                docker login -u ethosindia -p dckr_pat_4S0EcsM5lO5Z1gxDT-q5NUkKf4U
+            """
+        }
+
+        dockerBuildPush {
+            // by default, the step runs not only 'docker build' but also 'docker push'
+            // to disable pushing, add the following line:
+            // push = false
+
+            // path to Docker context (by default, context is working dir)
+            // context = "docker"
+            // path to Dockerfile relative to the project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+            file = "Dockerfile-kubernetes.proxy"
+            // build-time variables
+            // args["HTTP_PROXY"] = "http://10.20.30.2:1234"
+            // image labels
+            // labels["vendor"] = "mycompany"
+            // to add a raw list of additional build arguments, use
+            // extraArgsForBuildCommand = listOf("...")
+            // to add a raw list of additional push arguments, use
+            // extraArgsForPushCommand = listOf("...")
+            // image tags
+            val dockerHubRepo = "docker.io/ethosindia/eapp-capabilities-proxy-kubernetes"
+            tags {
+                // use current job run number as a tag - '0.0.run_number'
+                +"$dockerHubRepo:{{ VERSION_NUMBER }}"
+                +"$dockerHubRepo:latest"
+            }
+        }
+
+        requirements {
+            workerTags("windows-pool")
+        }
+    }
+}
+
 job("Build Nginx Upstream Image") {
     startOn {
         gitPush {
