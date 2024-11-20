81cc7ea95b9874adcc6f5166f29a6308083aecea
khetana <amit.khetan.70@50gramx.io>
2024-11-20 05:48:49 +0530

    refactored server backup

--- src/server_backup.py
+++ src/server_backup.py
@@ -27,22 +27,33 @@ import time
 from concurrent import futures
 
 import grpc
-from ethos.elint.services.product.identity.account_assistant.access_account_assistant_pb2_grpc import \
-    add_AccessAccountAssistantServiceServicer_to_server
-from ethos.elint.services.product.identity.account_assistant.action_account_assistant_pb2_grpc import \
-    add_ActionAccountAssistantServiceServicer_to_server
-from ethos.elint.services.product.identity.account_assistant.connect_account_assistant_pb2_grpc import \
-    add_ConnectAccountAssistantServiceServicer_to_server
-from ethos.elint.services.product.identity.account_assistant.create_account_assistant_pb2_grpc import \
-    add_CreateAccountAssistantServiceServicer_to_server
-from ethos.elint.services.product.identity.account_assistant.discover_account_assistant_pb2_grpc import \
-    add_DiscoverAccountAssistantServiceServicer_to_server
-from ethos.elint.services.product.identity.space.access_space_pb2_grpc import add_AccessSpaceServiceServicer_to_server
-from ethos.elint.services.product.identity.space.create_space_pb2_grpc import add_CreateSpaceServiceServicer_to_server
+from ethos.elint.services.product.identity.account_assistant.access_account_assistant_pb2_grpc import (
+    add_AccessAccountAssistantServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.account_assistant.action_account_assistant_pb2_grpc import (
+    add_ActionAccountAssistantServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.account_assistant.connect_account_assistant_pb2_grpc import (
+    add_ConnectAccountAssistantServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.account_assistant.create_account_assistant_pb2_grpc import (
+    add_CreateAccountAssistantServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.account_assistant.discover_account_assistant_pb2_grpc import (
+    add_DiscoverAccountAssistantServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.space.access_space_pb2_grpc import (
+    add_AccessSpaceServiceServicer_to_server,
+)
+from ethos.elint.services.product.identity.space.create_space_pb2_grpc import (
+    add_CreateSpaceServiceServicer_to_server,
+)
 
 import db_session
 from application_context import ApplicationContext
-from community.gramx.fifty.zero.ethos.identity.entities.account.handler import handle_account_services
+from community.gramx.fifty.zero.ethos.identity.entities.account.handler import (
+    handle_account_services,
+)
 from loader import Loader
 
 _LOGGER = logging.getLogger(__name__)
@@ -50,13 +61,15 @@ _LOGGER = logging.getLogger(__name__)
 _ONE_DAY = datetime.timedelta(days=1)
 _PROCESS_COUNT = multiprocessing.cpu_count()
 _THREAD_CONCURRENCY = _PROCESS_COUNT
-_MAX_WORKERS = int(os.environ['EAPP_SERVICE_IDENTITY_GRPC_MAX_WORKERS'])
+_MAX_WORKERS = int(os.environ["EAPP_SERVICE_IDENTITY_GRPC_MAX_WORKERS"])
 
-PORT = os.environ.get('EAPP_SERVICE_IDENTITY_PORT', None)
+PORT = os.environ.get("EAPP_SERVICE_IDENTITY_PORT", None)
 if PORT is None:
     logging.error("PORT NOT FOUND!")
 
-GRPC_MAX_CONNECTION_IDLE_MS = os.environ.get('EAPP_SERVICE_IDENTITY_GRPC_MAX_CONNECTION_IDLE_MS', None)
+GRPC_MAX_CONNECTION_IDLE_MS = os.environ.get(
+    "EAPP_SERVICE_IDENTITY_GRPC_MAX_CONNECTION_IDLE_MS", None
+)
 if GRPC_MAX_CONNECTION_IDLE_MS is None:
     GRPC_MAX_CONNECTION_IDLE_MS = 15000
 else:
@@ -74,11 +87,11 @@ max_workers = _MAX_WORKERS
 def run_server(port):
     # Initiate the DbSession
     db_session.DbSession.init_db_session()
-    logging.info(f'DbSession started')
+    logging.info(f"DbSession started")
 
     # Load Context
     Loader.init_multiverse_identity_context()
-    logging.info(f'Identity context loaded')
+    logging.info(f"Identity context loaded")
 
     # this channel argument controls the period (in milliseconds) after which a keepalive ping is sent on the transport.
     # grpc arg keepalive time ms, by-default, client disabled (INT_MAX), server 2 hours (7200000)
@@ -113,16 +126,14 @@ def run_server(port):
     # I will not alter this.
 
     options = (
-        ('grpc.max_connection_idle_ms', GRPC_MAX_CONNECTION_IDLE_MS),
+        ("grpc.max_connection_idle_ms", GRPC_MAX_CONNECTION_IDLE_MS),
         # ('grpc.max_connection_age_ms', 5000),
         # ('grpc.max_connection_age_grace_ms', 10000),
         # ('grpc.client_idle_timeout_ms', 3000),
     )
 
     # Bind ThreadPoolExecutor and Services to server
-    server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=max_workers)
-    )
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
 
     identity_layer = int(os.environ.get("EAPP_SERVICE_IDENTITY_LAYER", "0"))
     if identity_layer == 0:
@@ -138,14 +149,22 @@ def run_server(port):
     __add_machine_servicer_to_server(server)
 
     # TODO: Pass down the credentials to secure port
-    with open(os.environ['EAPP_SERVICE_IDENTITY_KEY'], 'rb') as f:
+    with open(os.environ["EAPP_SERVICE_IDENTITY_KEY"], "rb") as f:
         private_key = f.read()
 
-    with open(os.environ['EAPP_SERVICE_IDENTITY_COMMON_GRPC_EXTERNAL_CERTIFICATE_FILE'], 'rb') as f:
+    with open(
+        os.environ["EAPP_SERVICE_IDENTITY_COMMON_GRPC_EXTERNAL_CERTIFICATE_FILE"], "rb"
+    ) as f:
         certificate_chain = f.read()
 
     server_creds = grpc.ssl_server_credentials(
-        ((private_key, certificate_chain,),))
+        (
+            (
+                private_key,
+                certificate_chain,
+            ),
+        )
+    )
 
     server_port = server.add_insecure_port(f"[::]:{PORT}")
     # server_port = server.add_secure_port(f"[::]:{PORT}", server_creds)
@@ -203,40 +222,51 @@ def __add_account_assistant_servicer_to_server(server):
 
 def _run_server(bind_address):
     """Start a server in a subprocess."""
-    logging.info(f'Starting new server at {bind_address}')
-    options = (('grpc.so_reuseport', 1),)
+    logging.info(f"Starting new server at {bind_address}")
+    options = (("grpc.so_reuseport", 1),)
 
     # Bind ThreadPoolExecutor and Services to server
-    server = grpc.server(futures.ThreadPoolExecutor(
-        max_workers=_MAX_WORKERS, ),
-        options=options)
+    server = grpc.server(
+        futures.ThreadPoolExecutor(
+            max_workers=_MAX_WORKERS,
+        ),
+        options=options,
+    )
     #
     # server = grpc.server(
     #     futures.ThreadPoolExecutor(max_workers=max_workers)
     # )
-    logging.info(f'gRPC Server Created at {bind_address}')
+    logging.info(f"gRPC Server Created at {bind_address}")
 
     # Initiate the DbSession
     db_session.DbSession.init_db_session()
-    logging.info(f'DbSession started at {bind_address}')
+    logging.info(f"DbSession started at {bind_address}")
 
     # Load Context
     Loader.init_multiverse_identity_context()
-    logging.info(f'Identity context loaded at {bind_address}')
+    logging.info(f"Identity context loaded at {bind_address}")
 
     server = handle_account_services(server=server)
     __add_space_servicer_to_server(server)
     __add_account_assistant_servicer_to_server(server)
     __add_machine_servicer_to_server(server)
 
-    with open(os.environ['EAPP_SERVICE_IDENTITY_KEY'], 'rb') as f:
+    with open(os.environ["EAPP_SERVICE_IDENTITY_KEY"], "rb") as f:
         private_key = f.read()
 
-    with open(os.environ['EAPP_SERVICE_IDENTITY_COMMON_GRPC_EXTERNAL_CERTIFICATE_FILE'], 'rb') as f:
+    with open(
+        os.environ["EAPP_SERVICE_IDENTITY_COMMON_GRPC_EXTERNAL_CERTIFICATE_FILE"], "rb"
+    ) as f:
         certificate_chain = f.read()
 
     server_creds = grpc.ssl_server_credentials(
-        ((private_key, certificate_chain,),))
+        (
+            (
+                private_key,
+                certificate_chain,
+            ),
+        )
+    )
 
     # server_port = server.add_insecure_port(f"[::]:{PORT}")
     server_port = server.add_secure_port(bind_address, server_creds)
@@ -253,18 +283,22 @@ def _reserve_port():
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     if sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT) == 0:
         raise RuntimeError("Failed to set SO_REUSEPORT.")
-    sock.bind(('', 50501))
+    sock.bind(("", 50501))
     try:
-        logging.info(f"----------------------- Yielding Socket {sock.getsockname()[1]} -----------------------")
+        logging.info(
+            f"----------------------- Yielding Socket {sock.getsockname()[1]} -----------------------"
+        )
         yield sock.getsockname()[1]
     finally:
-        logging.info("----------------------- Closing Socket at finally -----------------------")
+        logging.info(
+            "----------------------- Closing Socket at finally -----------------------"
+        )
         sock.close()
 
 
 def main():
     with run_server(PORT) as (server, port):
-        logging.info(f'\tEthosApp Identity Server is listening at port {port}')
+        logging.info(f"\tEthosApp Identity Server is listening at port {port}")
         server.wait_for_termination()
     #
     # with _reserve_port() as port:
@@ -285,12 +319,11 @@ def main():
     #         worker.join()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # handler = logging.StreamHandler(sys.stdout)
     # formatter = logging.Formatter('[PID %(process)d] %(message)s')
     # handler.setFormatter(formatter)
     # _LOGGER.addHandler(handler)
     # _LOGGER.setLevel(logging.INFO)
-    logging.basicConfig(level=logging.INFO)  # warn: testing this
     main()
     # main()
