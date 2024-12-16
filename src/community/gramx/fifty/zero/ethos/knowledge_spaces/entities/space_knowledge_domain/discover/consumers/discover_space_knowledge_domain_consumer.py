01a3fbfaaaa5dd02b9ee91c3071c5ac45bb7976e
khetana <amit.khetan.70@50gramx.io>
2024-12-16 21:29:09 +0530

    refactored discover knowledge domain consumer

--- src/community/gramx/fifty/zero/ethos/knowledge_spaces/entities/space_knowledge_domain/discover/consumers/discover_space_knowledge_domain_consumer.py
+++ src/community/gramx/fifty/zero/ethos/knowledge_spaces/entities/space_knowledge_domain/discover/consumers/discover_space_knowledge_domain_consumer.py
@@ -16,11 +16,19 @@
 #   * is strictly forbidden unless prior written permission is obtained
 #   * from Amit Kumar Khetan.
 #   */
-from ethos.elint.entities.space_knowledge_domain_pb2 import SpaceKnowledgeDomain, RankedAnswer
-from ethos.elint.services.product.knowledge.space_knowledge_domain.access_space_knowledge_domain_pb2 import \
-    SpaceKnowledgeDomainServicesAccessAuthDetails
-from ethos.elint.services.product.knowledge.space_knowledge_domain.discover_space_knowledge_domain_pb2 import \
-    GetBestAnswersRequest
+import logging
+
+from ethos.elint.entities.space_knowledge_domain_pb2 import (
+    RankedAnswer,
+    SpaceKnowledgeDomain,
+)
+from ethos.elint.services.product.knowledge.space_knowledge_domain.access_space_knowledge_domain_pb2 import (
+    SpaceKnowledgeDomainServicesAccessAuthDetails,
+)
+from ethos.elint.services.product.knowledge.space_knowledge_domain.discover_space_knowledge_domain_pb2 import (
+    GetAllDomainFilesResponse,
+    GetBestAnswersRequest,
+)
 
 from application_context import ApplicationContext
 
@@ -28,18 +36,42 @@ from application_context import ApplicationContext
 class DiscoverSpaceKnowledgeDomainConsumer:
 
     @staticmethod
+    def get_all_domain_files(
+        skd_auth: SpaceKnowledgeDomainServicesAccessAuthDetails,
+    ) -> GetAllDomainFilesResponse:
+        stub = ApplicationContext.discover_space_knowledge_domain_service_stub()
+        logging.info(
+            f"DiscoverSpaceKnowledgeDomainConsumer:get_all_domain_files: {stub}"
+        )
+        logging.info(
+            f"DiscoverSpaceKnowledgeDomainConsumer:get_all_domain_files: {type(stub)}"
+        )
+        response = stub.GetAllDomainFiles(skd_auth)
+        return response
+
+    @staticmethod
     def get_updated_domain(
-            access_auth_details: SpaceKnowledgeDomainServicesAccessAuthDetails) -> SpaceKnowledgeDomain:
+        access_auth_details: SpaceKnowledgeDomainServicesAccessAuthDetails,
+    ) -> SpaceKnowledgeDomain:
         stub = ApplicationContext.discover_space_knowledge_domain_service_stub()
         response = stub.GetUpdatedDomain(access_auth_details)
         return response
 
     @staticmethod
-    def get_best_answers(access_auth_details: SpaceKnowledgeDomainServicesAccessAuthDetails,
-                         best_answers_count: int,
-                         question: str) -> (bool, str, [RankedAnswer]):
+    def get_best_answers(
+        access_auth_details: SpaceKnowledgeDomainServicesAccessAuthDetails,
+        best_answers_count: int,
+        question: str,
+    ) -> (bool, str, [RankedAnswer]):
         stub = ApplicationContext.discover_space_knowledge_domain_service_stub()
         request = GetBestAnswersRequest(
-            access_auth_details=access_auth_details, best_answers_count=best_answers_count, question=question)
+            access_auth_details=access_auth_details,
+            best_answers_count=best_answers_count,
+            question=question,
+        )
         response = stub.GetBestAnswers(request)
-        return response.response_meta.meta_done, response.response_meta.meta_message, response.ranked_answers
+        return (
+            response.response_meta.meta_done,
+            response.response_meta.meta_message,
+            response.ranked_answers,
+        )
